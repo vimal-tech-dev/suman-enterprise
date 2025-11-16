634d3ee24066759bd3963926889a1a42715dd2e3
vimal-tech-dev <be08it929@gmail.com>
2025-11-17 03:35:09 +0530

    initial commit from local repo
    Signed-off-by: vimal-tech-dev <be08it929@gmail.com>

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="text-center">
+        <v-alert type="success" variant="tonal">
+          Vuetify 3 + TypeScript is working! 🎉
+        </v-alert>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+// No logic needed for now
+</script>
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,12 @@
+// src/plugins/vuetify.ts
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+export default vuetify
